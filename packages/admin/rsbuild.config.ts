18db32162c9d8105fc3be21b831e77eeb95e7447
zhenghui-su <343196323@qq.com>
2025-08-05 12:43:20 +0800

    feat: 添加网站图标并更新相关配置

--- packages/admin/rsbuild.config.ts
+++ packages/admin/rsbuild.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  html: {
+    title: '加藤惠网站管理后台',
+    favicon: './public/katou-megumi.ico',
+  },
   server: {
     port: 3002,
   },
--- packages/web/rsbuild.config.ts
+++ packages/web/rsbuild.config.ts
@@ -2,28 +2,31 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  html: {
-    template: './index.html',
-  },
-  source: {
-    entry: {
-      index: './src/main.tsx',
-    },
-    alias: {
-      '@': './src',
-    },
-  },
-  output: {
-    distPath: {
-      root: 'dist',
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  dev: {
-    hmr: true,
-  },
+	plugins: [pluginReact()],
+	html: {
+		template: './index.html',
+		favicon: './public/katou-megumi.ico',
+	},
+	source: {
+		entry: {
+			index: './src/main.tsx',
+		},
+	},
+	resolve: {
+		alias: {
+			'@': './src',
+		},
+	},
+	output: {
+		distPath: {
+			root: 'dist',
+		},
+	},
+	server: {
+		port: 3000,
+		open: true,
+	},
+	dev: {
+		hmr: true,
+	},
 });
